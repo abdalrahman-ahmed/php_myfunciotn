1de71f6025e0ea4b86faf8c0f7cb55f970f671a6
Ahmed-Shahen <ahmad.alshahen@gmail.com>
2013-10-09 18:26:00 +0200

    Create clean_text.php

---
+++ clean_text.php
@@ -0,0 +1,66 @@
+<?php
+ 
+/*
+ @ Clean Text Function PHP
+ @ Author      : Ahmed Shahen
+ @ Author url  : https://github.com/Ahmed-Shahen
+ @ License     : GNU GENERAL PUBLIC LICENSE  
+ @ Github url  : https://github.com/Ahmed-Shahen/php_myfunciotn	
+ @ version     : 1.0.0
+
+*/
+
+# EXAMPLE
+
+/*
+ clean_text("hi th*(&%#@is is *^&%#!<>,.;-+{]test","",0);  // return   1
+
+ clean_text("hi th*(&%#@is is *^&%#!<>,.;-+{]test","",1);  // return   hi this is test 
+	
+ clean_text("hi this is test !? ","!,#,^,",2);  		   // return   1   	
+
+ clean_text("hi this is test !? ","!,#,^,",3);             // return   hi this is test ?
+
+*/
+
+
+
+function clean_text($string,$dirty_string,$mod){
+$value =  array('!','@','#','$','%','^','&','*','(',')','-','_','=','+','|','{','}','[',']','؛','‘','ّ','~',',','<','>','.','؟','?','/',':',';',"'",'"','`');
+if ($mod == 0) {
+foreach ($value as $values) {	
+if (stristr($string,$values)) {
+	return 1;
+	exit;
+}
+}
+}
+if ($mod == 1) {
+$clean = str_replace($value,'',$string);
+return $clean;
+exit;
+}
+if ($mod == 2) {
+if (!empty($dirty_string)) {
+$value = explode(',',$dirty_string);	
+foreach ($value as $values) {	
+if (stristr($string,$values)) {
+	return 1;
+	exit;
+}
+}	
+}
+}
+if ($mod == 3) {
+if (!empty($dirty_string)) {
+$value = explode(',',$dirty_string);
+$clean = str_replace($value,'',$string);
+return $clean;
+exit;
+}	
+}
+}
+
+
+
+?>
