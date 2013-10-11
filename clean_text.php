981ced243639c03427389a54062a9c9cf679101e
DevAhmedElsayed <ahmed_thetop@hotmail.com>
2013-10-11 14:46:55 -0700

    Update clean_text.php
    clean code meaning maintainable code .

--- clean_text.php
+++ clean_text.php
@@ -23,44 +23,39 @@
 
 */
 
-
-
 function clean_text($string,$dirty_string,$mod){
-$value =  array('!','@','#','$','%','^','&','*','(',')','-','_','=','+','|','{','}','[',']','؛','‘','ّ','~',',','<','>','.','؟','?','/',':',';',"'",'"','`');
-if ($mod == 0) {
-foreach ($value as $values) {	
-if (stristr($string,$values)) {
-	return 1;
-	exit;
+	$value =  array('!','@','#','$','%','^','&','*','(',')','-','_','=','+','|','{','}','[',']','؛','‘','ّ','~',',','<','>','.','؟','?','/',':',';',"'",'"','`');
+	if ($mod == 0) {
+		foreach ($value as $values) {	
+			if (stristr($string,$values)) {
+				return 1;
+				exit;
+			}
+		}
+	}
+	if ($mod == 1) {
+		$clean = str_replace($value,'',$string);
+		return $clean;
+		exit;
+	}
+	if ($mod == 2) {
+		if (!empty($dirty_string)) {
+		$value = explode(',',$dirty_string);	
+			foreach ($value as $values) {	
+				if (stristr($string,$values)) {
+					return 1;
+					exit;
+				}
+			}	
+		}
+	}
+	if ($mod == 3) {
+		if (!empty($dirty_string)) {
+		$value = explode(',',$dirty_string);
+		$clean = str_replace($value,'',$string);
+		return $clean;
+		exit;
+		}	
+	}
 }
-}
-}
-if ($mod == 1) {
-$clean = str_replace($value,'',$string);
-return $clean;
-exit;
-}
-if ($mod == 2) {
-if (!empty($dirty_string)) {
-$value = explode(',',$dirty_string);	
-foreach ($value as $values) {	
-if (stristr($string,$values)) {
-	return 1;
-	exit;
-}
-}	
-}
-}
-if ($mod == 3) {
-if (!empty($dirty_string)) {
-$value = explode(',',$dirty_string);
-$clean = str_replace($value,'',$string);
-return $clean;
-exit;
-}	
-}
-}
-
-
-
 ?>
